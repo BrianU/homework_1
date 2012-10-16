f1e83d42b03d5f9d2a6731b81816b1ab77516021
saasbook <saasbook@saasbook.(none)>
2012-10-16 12:36:00 -0700

    moving over to the windows box for dev

--- part1.rb
+++ part1.rb
@@ -16,5 +16,28 @@ def count_words(str)
       word_counts[word] += 1
     end
   end
-  word_counts
+  return word_counts
 end
+
+#the code below this line will test your functions.  You should remove everything below this line prior to submitting your file
+
+
+test_str = "there goes the neighborhood"
+
+if palindrome? test_str
+  puts test_str + " is a palindrome!"
+else
+  puts test_str + " is NOT a palindrome!"
+end
+
+test_str = "Madam, I'm Adam"
+if palindrome? test_str
+  puts test_str + " is a palindrome!"
+else
+  pura test_str + " is NOT a palindrome"
+end
+
+test_str = "The rent is due on the first day of the month unless the first day of the month falls on a Saturday or Sunday"
+
+word_count = count_words test_str
+puts word_count
--- part2.rb
+++ part2.rb
@@ -3,12 +3,69 @@ class NoSuchStrategyError < StandardError ; end
 
 def rps_result(m1, m2)
   # YOUR CODE HERE
+  if ( m1[1] == m2[1])
+    return m1
+  end
+  
+  if ( m1[1].downcase=='r' and m2[1].downcase=='s')
+    return m1
+  end
+  
+  if ( m1[1].downcase=='r' and m2[1].downcase=='p')
+    return m2
+  end
+  
+  if ( m1[1].downcase=='s' and m2[1].downcase=='r')
+    return m2
+  end
+  
+  if ( m1[1].downcase=='s' and m2[1].downcase=='p')
+    return m1
+  end
+  
+  if ( m1[1].downcase=='p' and m2[1].downcase=='s')
+    return m2
+  end
+  if ( m1[1].downcase=='p' and m2[1].downcase=='r')
+    return m1
+  end
 end
 
 def rps_game_winner(game)
-  # YOUR CODE HERE
+  raise WrongNumberOfPlayersError unless game.length == 2
+#  raise NoSuchStrategyError unless 
+#    game[0][1].downcase =~ /s|p|r/ and game[1][1].downcase =~ /s|p|r/ 
+  winner = rps_result(game[0], game[1])
+  return winner
 end
 
 def rps_tournament_winner(tournament)
-  # YOUR CODE HERE
+  tournament.each do |game|
+    puts rps_game_winner(game)
+  end
 end
+
+game = [["Armando", "s"], ["Dave", "W"]]
+puts rps_game_winner( game )
+
+#game = [["Armando", "P"], ["Dave", "P"], ["Arse", "R"]]
+#rps_game_winner( game )
+
+rps_game_winner( game )
+ 
+game = [["Armando", "P"], ["Dave", "W"]]
+ 
+rps_game_winner( game )
+
+tournament =  [
+                  [
+                      [ ["Armando", "P"], ["Dave", "S"] ],
+                      [ ["Richard", "R"],  ["Michael", "S"] ],
+                  ],
+                  [
+                      [ ["Allen", "S"], ["Omer", "P"] ],
+                      [ ["David E.", "R"], ["Richard X.", "P"] ]
+                  ]
+              ]
+              
+# rps_tournament_winner tournament               
